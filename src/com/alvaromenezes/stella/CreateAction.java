a996d4e77b155aaf4b042d2b337c9020d5f74375
alvaromenezes <alvaromenezes.inf@gmail.com>
2017-05-16 15:28:00 -0300

    initial commit

---
+++ src/com/alvaromenezes/stella/CreateAction.java
@@ -0,0 +1,15 @@
+package com.alvaromenezes.stella;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by alvaromenezes on 5/16/17.
+ */
+public class CreateAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
