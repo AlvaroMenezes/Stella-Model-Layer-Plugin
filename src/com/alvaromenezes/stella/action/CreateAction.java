dcfde43b160ca1d3a99ed41abd7b593340e24f38
alvaromenezes <alvaromenezes.inf@gmail.com>
2017-05-17 21:39:53 -0300

    Add view

---
+++ src/com/alvaromenezes/stella/action/CreateAction.java
@@ -0,0 +1,24 @@
+package com.alvaromenezes.stella.action;
+
+import com.alvaromenezes.stella.view.StellaForm;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+import javax.swing.*;
+
+/**
+ * Created by alvaromenezes on 5/16/17.
+ */
+public class CreateAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        JFrame frame = new JFrame("Stella Model Layer");
+        StellaForm form = new StellaForm();
+        frame.setContentPane(form.panelMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
---
+++ src/com/alvaromenezes/stella/view/StellaForm.java
@@ -0,0 +1,14 @@
+package com.alvaromenezes.stella.view;
+
+import javax.swing.*;
+
+/**
+ * Created by alvaromenezes on 5/17/17.
+ */
+public class StellaForm {
+    public JPanel panelMain;
+    private JTextField txtPath;
+    private JTextField txtURL;
+    private JButton btnPath;
+    private JButton btnGenerate;
+}
